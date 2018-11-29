1277ed5b641733ff8d2ebf8e6271783958a88275
Aya Kawasumi <sxa.1105bo@gmail.com>
2018-11-29 15:34:17 +0900

    配列をソート

--- sort.js
+++ sort.js
@@ -1,2 +1,12 @@
+sortNum = (num) => {
+	num.sort((a, b) => {
+		if(a === b) {
+			return 0
+		}
+		return a < b ? -1 : 1
+	)}
+}
+
 const num = [19, 3, 81, 1, 24, 21]
+sortNum(num)
 console.log(num)
